986804a7cc062949c65b18cc02fcc3c84a856422
John Eady <john.eady@gmail.com>
2025-11-22 18:29:15 -0500

    partial test fix

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,14 +2,31 @@
 
 namespace Tests;
 
+use Illuminate\Contracts\Console\Kernel;
+use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\ViewErrorBag;
 
 abstract class TestCase extends BaseTestCase
 {
+    public function createApplication(): Application
+    {
+        $app = require __DIR__.'/../bootstrap/app.php';
+
+        $app->make(Kernel::class)->bootstrap();
+
+        return $app;
+    }
+
     protected function setUp(): void
     {
         parent::setUp();
 
-        $this->withoutMiddleware(\Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class);
+        $this->withoutVite();
+        $this->withoutMiddleware();
+
+        // Share an empty error bag with all views for Flux components
+        View::share('errors', new ViewErrorBag);
     }
 }
