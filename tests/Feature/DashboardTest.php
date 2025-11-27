ae3de51a4ad5f1b173efd4a8f8b2cc82c8618b97
John Eady <john.eady@gmail.com>
2025-11-26 20:37:16 -0500

    misc

--- tests/Feature/DashboardTest.php
+++ tests/Feature/DashboardTest.php
@@ -2,12 +2,11 @@
 
 use App\Models\User;
 
+test('guests are redirected to the login page', function () {
+    $this->get('/dashboard')->assertRedirect('/login');
+});
+
 // TODO this needs to be-fixed
-
-// test('guests are redirected to the login page', function () {
-//     $this->get('/dashboard')->assertRedirect('/login');
-// });
-
 // test('authenticated users can visit the dashboard', function () {
 //     $this->actingAs($user = User::factory()->create());
 
